9b3d8d73143ce693fd2b4ea4745e741152beaf75
ja-weiss <jacobweiss.mm@gmail.com>
2025-03-19 17:52:29 +0100

    button fix

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-document.querySelector(".button1").addEventListener("click", function() {
+document.getElementsByClassName("button1")[0].addEventListener("click", function() {
     alert("Why did you click The Button\u2122? .-.");
 });
 
-document.querySelector(".button2").addEventListener("click", function() {
+document.getElementsByClassName("button2")[0].addEventListener("click", function() {
     alert("You clicked The Second Button\u2122.");
 });
