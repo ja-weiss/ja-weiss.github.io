fd7bbee3c91d691d4592e8e25c165db6c1b6b032
ja-weiss <jacobweiss.mm@gmail.com>
2025-03-20 17:27:50 +0100

    to-do list

--- script/buttons.js
+++ script/buttons.js
@@ -3,10 +3,17 @@ document.getElementsByClassName("button1")[0].addEventListener("click", function
     alert("Why did you click The Button\u2122? .-.");
 });
 
-/* Button 2 */
-document.getElementsByClassName("button2")[0].addEventListener("click", function() {
-    alert("You clicked The Second Button\u2122.");
+document.querySelector(".button2").addEventListener("click", function() {
+    document.getElementById("alertMessage").innerHTML = 
+        "- number generator<br>- minigame<br><br>" +
+        "<s>- spotify embed<br>- language switcher<br>- dark mode<br>- clock</s>";
+    document.getElementById("customAlert").style.display = "block";
 });
+
+function closeAlert() {
+    document.getElementById("customAlert").style.display = "none";
+}
+
 
 /* Button 3 */
 document.querySelector(".button3").addEventListener("click", function() {
